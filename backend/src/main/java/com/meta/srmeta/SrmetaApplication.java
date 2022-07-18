46dfda6ccd18927cb50a645fc42cc452cd9c78f4
nathanjfa <nathan.farias@dcx.ufpb.br>
2022-07-18 19:01:49 -0300

    first commit

---
+++ backend/src/main/java/com/meta/srmeta/SrmetaApplication.java
@@ -0,0 +1,13 @@
+package com.meta.srmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/meta/srmeta/SrmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meta.srmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
